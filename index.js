e69067ff529d2e921ad42d7a2f09e1dc373afdf1
elliotjeisenberg <elliotjeisenberg@gmail.com>
2022-12-28 22:22:37 -0600

    push changes to internet

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('postdata', ((req, res) => { return JSON.stringify(req.body)}))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'))
 
